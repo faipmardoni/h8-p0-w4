758f2dfd025f38e19bc71800e0990d211c331d6c
Faip Mardoni <faipmardoni@gmail.com>
2018-03-13 17:53:24 +0700

    bilangan prima

---
+++ exercise-1.js
@@ -0,0 +1,25 @@
+function angkaPrima(angka) {
+    // you can only write your code here!
+    var prime=false;
+    if(angka&2!==0&&angka>2) {
+        for(var i=2; i<angka; i++) {
+            if(angka%i==0) {
+                return false;
+            }else {
+                prime=true;
+            }
+        }
+    }else if(angka==2) {
+        return true;
+    }else {
+        return false;
+    }
+    return prime;
+  }
+  
+  // TEST CASES
+  console.log(angkaPrima(3)); // true
+  console.log(angkaPrima(7)); // true
+  console.log(angkaPrima(6)); // false
+  console.log(angkaPrima(23)); // true
+  console.log(angkaPrima(33)); // false
