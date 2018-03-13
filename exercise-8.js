75dd6b191bb6274eb0899f37ca8a19d6853ef875
Faip Mardoni <faipmardoni@gmail.com>
2018-03-13 23:52:28 +0700

    urutkan Abjad

---
+++ exercise-8.js
@@ -0,0 +1,22 @@
+function urutkanAbjad(str) {
+    // you can only write your code here!
+    var arrB = [];
+    for (var i = 0; i < str.length; i++) {
+        var hurufToNum = str[i].charCodeAt(0)+95;
+        arrB.push(hurufToNum);
+    }
+    arrB.sort();
+    var arrC = ''
+    for (var j = 0; j < arrB.length; j++) {
+        var numToHuruf = String.fromCharCode(arrB[j]-95);
+        arrC+=numToHuruf;
+    }
+    return arrC;
+  }
+  
+  // TEST CASES
+  console.log(urutkanAbjad('hello')); // 'ehllo'
+  console.log(urutkanAbjad('truncate')); // 'acenrttu'
+  console.log(urutkanAbjad('developer')); // 'deeeloprv'
+  console.log(urutkanAbjad('software')); // 'aeforstw'
+  console.log(urutkanAbjad('aegis')); // 'aegis'
